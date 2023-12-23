372a2a673f90676eeed7839e0d241f16e9fb32dc
harshal-ganbote <harshal.ganbote@outlook.com>
2023-12-23 08:15:52 +0530

    module change

--- src/modules/M4_Introduction_to_Problem_Solving_Intermediate_2/Intermediate_DSA/L12_Maths_Modular_Arthimetic_Introduction/ModLargeNumber.java
+++ src/modules/M4_Introduction_to_Problem_Solving_Intermediate_2/Intermediate_DSA/L12_Maths_Modular_Arthimetic_Introduction/ModLargeNumber.java
@@ -1,4 +1,4 @@
-package modules.M4_Introduction_to_Problem_Solving_Intermediate_2.L12_Maths_Modular_Arthimetic_Introduction;
+package modules.M4_Introduction_to_Problem_Solving_Intermediate_2.Intermediate_DSA.L12_Maths_Modular_Arthimetic_Introduction;
 
 import java.util.Arrays;
 
