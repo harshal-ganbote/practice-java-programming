465328ee7b9b11ca8980e14cb7c9066872b3d6bb
harshal-ganbote <harshal.ganbote@outlook.com>
2024-05-11 10:31:25 +0530

    sql details added

--- src/modules/M9_Databases_and_SQL/SQL/L2_Keys/practice.sql
+++ src/modules/M9_Databases_and_SQL/SQL/L2_Keys/practice.sql
@@ -1,53 +1,71 @@
-
 -- Scaler Database
 -- Students Tables
 -- Batches Tables 
-
 CREATE DATABASE scalerDB;
-
+-- 
 USE scalerDB;
-
+--
 -- Create Batches Table
 CREATE TABLE batches(id INT PRIMARY KEY, name varchar(50));
-
+--
 -- Insert data into batch tables 
-INSERT INTO batches(id, name) VALUES 
-(1, "ML"),
-(2, "SQL"),
-(3, "DSA");
-
+INSERT INTO batches(id, name)
+VALUES (1, "ML"),
+  (2, "SQL"),
+  (3, "DSA");
+--
 -- See all the rows of a batches table
-SELECT * FROM batches;
-
+SELECT *
+FROM batches;
+--
 -- Create Students Table 
 CREATE TABLE students(
-id INT PRIMARY KEY AUTO_INCREMENT,
-first_name VARCHAR(50),
-last_name VARCHAR(50),
-batch_id INT,
-FOREIGN KEY(batch_id) REFERENCES batches(id) 
-ON DELETE CASCADE ON UPDATE CASCADE);
-
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  first_name VARCHAR(50),
+  last_name VARCHAR(50),
+  batch_id INT,
+  FOREIGN KEY(batch_id) REFERENCES batches(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+--
 -- Insert data into student tables 
-INSERT INTO students(first_name,last_name,batch_id) VALUES
-("Khushboo","Singh",2),
-("Mangesh","Kr",2),
-("Ashwani","Kr",3),
-("Harsh","Raj",1);
-
+INSERT INTO students(first_name, last_name, batch_id)
+VALUES ("Khushboo", "Singh", 2),
+  ("Mangesh", "Kr", 2),
+  ("Ashwani", "Kr", 3),
+  ("Harsh", "Raj", 1);
+--
 -- See all the rows of a student table
-SELECT * FROM students;
-
+SELECT *
+FROM students;
+--
 -- DELETION
-DELETE FROM batches WHERE id = 2;
-
+DELETE FROM batches
+WHERE id = 2;
+--
 -- Update
 UPDATE batches
 SET id = 100
 WHERE id = 1;
-
+--
 -- See all the rows of a student table
-select * from students;
-
+select *
+from students;
+--
 -- See all the rows of a batches table
-select * from batches;
+select *
+from batches;
+--
+-- for alter the foreign key constraints 
+ALTER TABLE students DROP FOREIGN KEY students_ibfk_1;
+--
+ALTER TABLE students
+ADD CONSTRAINT students_ibfk_1 FOREIGN KEY (batch_id) REFERENCES batches (id) ON UPDATE
+SET NULL ON DELETE
+SET NULL;
+--
+--
+SELECT CONSTRAINT_NAME
+FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE
+WHERE TABLE_SCHEMA = 'scalerDB'
+  AND TABLE_NAME = 'students'
+  AND COLUMN_NAME = 'batch_id';
